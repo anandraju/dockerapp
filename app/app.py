c5b42f9e46b490c228bce8a3744012ca8bedb288
root <root@ip-172-31-47-123.us-east-2.compute.internal>
2019-05-06 02:39:17 +0000

    updating all files

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, render_template
-import redis
 
 app = Flask(__name__)
 default_key = '1'
-cache = redis.StrictRedis(host='redis', port=6379, db=0)
-cache.set(default_key, "one")
+cache = {default_key: 'one'}
 
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
@@ -14,11 +12,11 @@ def mainpage():
 	    key = request.form['key']
 
 	if request.method == 'POST' and request.form['submit'] == 'save':
-		cache.set(key, request.form['cache_value'])
+		cache[key] = request.form['cache_value']
 
 	cache_value = None;
-	if cache.get(key):
-		cache_value = cache.get(key).decode('utf-8')
+	if key in cache:
+		cache_value = cache[key]
 
 	return render_template('index.html', key=key, cache_value=cache_value)
 
